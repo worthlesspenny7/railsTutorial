e4e742e310a08e9f5b1e32d8c71a09ca362b0531
Joshua Hogge <josh.hogge@gmail.com>
2014-02-23 19:57:59 -0700

    removing parent folder

---
+++ first_app/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FirstApp::Application.config.secret_key_base = '5b9981fa30aa95ec534eb7db4eb4fce11ead7b39321bb71445dce89667bf4318a39321c939d2e91b0bcd789422c8a832c5454eaee1850a7af881dd26afc8d1a5'
